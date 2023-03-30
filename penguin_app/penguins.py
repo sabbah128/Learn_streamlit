d7262591d62c2f9d0338d58bab6db5d1865980a1
Kian <sabbah128@gmail.com>
2023-03-30 11:51:35 -0700

    other change penguins.py

--- penguin_app/penguins.py
+++ penguin_app/penguins.py
@@ -13,21 +13,19 @@ st.write(penguins_df.head())
 species_list = list(penguins_df['species'].unique())
 
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
-selected_species = st.selectbox('What species would you like to visualize?', species_list)
+# selected_species = st.selectbox('What species would you like to visualize?', species_list)
 
-selected_var = [n for n in penguins_df.columns 
-                if n not in ['species', 'island', 'sex', 'year']]
+# selected_var = [n for n in penguins_df.columns 
+#                 if n not in ['species', 'island', 'sex', 'year']]
+# selected_x_var = st.selectbox('What do want the x variable to be?', selected_var)
+# selected_y_var = st.selectbox('What about the y?', selected_var)
 
-selected_x_var = st.selectbox('What do want the x variable to be?', selected_var)
-shuffle(selected_var)
-selected_y_var = st.selectbox('What about the y?', selected_var)
-
-# selected_x_var = st.selectbox('What do want the x variable to be?', 
-#                               ['bill_length_mm', 'bill_depth_mm', 
-#                                'flipper_length_mm', 'body_mass_g'])
-# selected_y_var = st.selectbox('What about the y?', 
-#                               ['bill_depth_mm', 'bill_length_mm', 
-#                                'flipper_length_mm', 'body_mass_g'])
+selected_x_var = st.selectbox('What do want the x variable to be?', 
+                              ['bill_length_mm', 'bill_depth_mm', 
+                               'flipper_length_mm', 'body_mass_g'])
+selected_y_var = st.selectbox('What about the y?', 
+                              ['bill_depth_mm', 'bill_length_mm', 
+                               'flipper_length_mm', 'body_mass_g'])
 # penguins_df = penguins_df[penguins_df['species'] == selected_species]
 
 sns.set_style('darkgrid')
@@ -45,5 +43,5 @@ ax = sns.scatterplot(data = penguins_df,
 plt.xlabel(selected_x_var)
 plt.ylabel(selected_y_var)
 
-plt.title(f'Scatterplot of {selected_species} Penguins')
+# plt.title(f'Scatterplot of {selected_species} Penguins')
 st.pyplot(fig)
