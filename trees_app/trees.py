067c6a0b23efbb0631dbb0a9006872f84075f4cb
Kian <sabbah128@gmail.com>
2023-03-30 14:12:50 -0700

    Update trees.py

--- trees_app/trees.py
+++ trees_app/trees.py
@@ -7,6 +7,7 @@ st.write("This app analyses trees in San Francisco using"
          " a dataset kindly provided by SF DPW")
 
 trees_df = pd.read_csv("trees.csv")
+st.write(trees_df.head())
 
 df_dbh_grouped = pd.DataFrame(trees_df.groupby(["dbh"]).count()["tree_id"])
 df_dbh_grouped.columns = ["tree_count"]
