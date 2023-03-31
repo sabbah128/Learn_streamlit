1f54173c6c7ce3edf4af1f1f89fe7b48d63ac0c4
Kian <sabbah128@gmail.com>
2023-03-31 06:22:51 -0700

    update trees.py
    calculate now time - tree was planted

--- trees_app/trees.py
+++ trees_app/trees.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
 
 
 st.title('SF Trees')
-st.write('This app analyses trees in San Francisco using'
-         ' a dataset kindly provided by SF DPW')
+st.write('This app analyses trees in San Francisco using a dataset kindly provided by SF DPW')
 
-st.subheader('Plotly Chart')
 trees_df = pd.read_csv('trees.csv')
+st.write(trees_df.head())
 
-fig = px.histogram(trees_df['dbh'])
-st.plotly_chart(fig)
+trees_df['age'] = (pd.to_datetime('today') - pd.to_datetime(trees_df['date'])).dt.days
+
+st.subheader('Seaborn Chart')
+fig_sb, ax_sb = plt.subplots()
+ax_sb = sns.histplot(trees_df['age'])
+plt.xlabel('Age (Days)')
+st.pyplot(fig_sb)
+
+st.subheader('Matploblib Chart')
+fig_mpl, ax_mpl = plt.subplots()
+ax_mpl = plt.hist(trees_df['age'], bins = 'auto', edgecolor = 'black')
+plt.xlabel('Age (Days)')
+st.pyplot(fig_mpl)
 
