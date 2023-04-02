82357be4a52c62dc013834224a291ac3552a08c9
Kian <sabbah128@gmail.com>
2023-04-02 06:37:38 -0700

    Update penguins_upload.py

--- penguin_app/penguins_upload.py
+++ penguin_app/penguins_upload.py
@@ -7,11 +7,13 @@ import seaborn as sns
 st.title("Palmer's Penguins")
 st.markdown('Use this Streamlit app to make your own scatterplot about penguins!')
 
-penguin_file = st.file_uploader( 'Select Your Local Penguins CSV (default provided)')
-if penguin_file is not None:
-    penguins_df = pd.read_csv(penguin_file)
-else:
-    penguins_df = pd.read_csv('penguins.csv')
+penguins_df = st.file_uploader( 'Select Your Local Penguins CSV (default provided)')
+if penguins_df is None:
+    st.write('Please upload your file')
+    st.stop()
+#     penguins_df = pd.read_csv(penguin_file)
+# else:
+#     penguins_df = pd.read_csv('penguins.csv')
 
 selected_x_var = st.selectbox('What do want the x variable to be?', 
                               ['bill_length_mm', 'bill_depth_mm', 
