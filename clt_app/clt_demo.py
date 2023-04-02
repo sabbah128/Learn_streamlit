312e64b869a41e1a2d7020c025816aab446eecd1
Kian <sabbah128@gmail.com>
2023-04-02 05:53:16 -0700

    Update clt_demo.py

--- clt_app/clt_demo.py
+++ clt_app/clt_demo.py
@@ -16,12 +16,12 @@ perc_heads = st.number_input(label = '**Chance of Coins Landing on Heads**',
                              max_value = 1.0, 
                              value = .5)
 
-graph_title = st.text_input(label='**Graph Title**')
+# graph_title = st.text_input(label='**Graph Title**')
 
 binom_dist = np.random.binomial(1, perc_heads, 1000)
 list_of_means = []
 
-for i in range(0, 500):
+for i in range(0, 1000):
     list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 
 fig, ax = plt.subplots()
