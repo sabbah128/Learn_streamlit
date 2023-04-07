e9b921b82869a6d5445e04643f3d2793bf7bf707
Kian <sabbah128@gmail.com>
2023-04-07 03:12:38 -0700

    Update goodreads_app.py

--- streamlit_goodreads_book/goodreads_app.py
+++ streamlit_goodreads_book/goodreads_app.py
@@ -24,3 +24,39 @@ else:
     st.write('Analyzing your Goodreads history')
 
 st.write(books_df.head())
+
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+books_df['Year Finished'] = pd.to_datetime(books_df['Date Read']).dt.year
+books_per_year = books_df.groupby('Year Finished')['Book Id'].count().reset_index()
+books_per_year.columns = ['Year Finished', 'Count']
+
+fig, ax = plt.subplots()
+
+ax = sns.barplot(data=books_per_year, 
+                            x='Year Finished', 
+                            y='Count')
+
+st.pyplot(fig)
+
+books_df['days_to_finish'] = (
+    pd.to_datetime(books_df['Date Read']) - pd.to_datetime(books_df['Date Added'])
+    ).dt.days
+
+books_finished_filtered = books_df[
+    (books_df['Exclusive Shelf'] == 'read') & (books_df['days_to_finish'] >= 0)]
+
+fig_days_finished = px.histogram(books_finished_filtered, 
+                                 x='days_to_finish',
+                                 title='Time Between Date Added And DateFinished',
+                                 labels={'days_to_finish':'days'})
+
+st.plotly_chart(fig_days_finished)
+
+books_rated = books_df[books_df['My Rating'] != 0]
+fig_my_rating = px.histogram(books_rated, x='My Rating', title='User Rating')
+st.plotly_chart(fig_my_rating)
+fig_avg_rating = px.histogram(books_rated, x='Average Rating', title='Average Goodreads Rating')
+st.plotly_chart(fig_avg_rating)
